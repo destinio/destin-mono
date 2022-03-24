b927abbe0420b026e45961998336005028726324
Destin Lee <destin.lee@johnsonfit.com>
2022-03-24 14:37:35 -0500

    docs: updated id function docs

--- packages/id/src/index.ts
+++ packages/id/src/index.ts
@@ -1,3 +1,7 @@
+/**
+ *
+ * @returns string - random string to use for id
+ */
 function id(): string {
   const randomNumber = String(Math.floor(Math.random() * 1000000000)).padStart(10, '0')
   return `${randomNumber}-${Date.now()}`
