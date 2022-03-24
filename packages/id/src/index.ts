404ecb1092025231d1c8983c4dd8c467b609793f
Destin Lee <destin.lee@johnsonfit.com>
2022-03-24 11:36:32 -0500

    clean2

--- packages/id/src/index.ts
+++ packages/id/src/index.ts
@@ -1,5 +1,7 @@
 function id(): string {
   const randomNumber = String(Math.floor(Math.random() * 1000000000)).padStart(10, '0')
 
-  return `${randomNumber}`
+  return `${randomNumber}-${Date.now()}`
 }
+
+export { id }
